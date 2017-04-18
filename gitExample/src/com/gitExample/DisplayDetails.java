a69eab2442cedd8a1e82f7ab6b4ad75b96971cf4
prasathdave <prasath.slipin@gmail.com>
2017-04-18 12:34:32 +0530

    Added DisplayDetails obj

---
+++ gitExample/src/com/gitExample/DisplayDetails.java
@@ -0,0 +1,10 @@
+package com.gitExample;
+
+public class DisplayDetails {
+	
+	public void display()
+	{
+		System.out.println("Hello here!!");
+	}
+
+}
--- gitExample/src/com/gitExample/Example.java
+++ gitExample/src/com/gitExample/Example.java
@@ -2,6 +2,8 @@ package com.gitExample;
 
 public class Example {
 	public static void main(String[] args){
+		DisplayDetails d=new DisplayDetails();
+		d.display();
 		System.out.println("hello world!");
 		System.out.println("success1");
 		System.out.println("success2");
